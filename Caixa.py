6ecb357fe590b6e2f3734f925fba11f219bbb367
pedro <pedrohazevedo@gmail.com>
2022-10-25 15:37:29 -0300

    Adições: - consulta por título - mostrar estoque na consulta - baixar estoque - corrigido erro de inserçao de total na venda - adicionado menu de consulta

--- Caixa.py
+++ Caixa.py
@@ -14,32 +14,79 @@ def menu():
     while True:
         if keyboard.is_pressed('1'):
             cls()
-            consulta()
+            erase()
+            Consulta()
             break
         elif keyboard.is_pressed('2'):
             Venda()
             cls()
+            break
 
-def consulta():
-    try:
-        con2 = sqlite3.connect('db.db')
-        cur2 = con2.cursor()
+class Consulta():
+
+    def __init__(self):
+        self.con = sqlite3.connect('db.db')
+        self.cur = con.cursor()
+        self.menu_consulta()
+
+    def menu_consulta(self):
+        cls()
+        print('''Como deseja consultar?\n
+        I. ISBN\n
+        T. Título''')
+
+        while True:
+            if keyboard.is_pressed('I'):
+                self.isbn()
+                break
+
+            elif keyboard.is_pressed('T'):
+                self.titulo()
+                break
+
+
+    def isbn(self):
+        cls()
         erase()
-        isbn = input('Leia o código de barras\n')
-        query = cur2.execute(f'SELECT descricao, valor FROM estoque where isbn={isbn}')
-        result = query.fetchone()
-        cls()
-        print(f'Título: {result[0]}\nPreço: R$ {result[1]}')
-        cur2.close()
-        con2.close()
-        time.sleep(3)
-        cls()
-        menu()
-    except:
-        print('Código de barras inválido. Tente novamente.')
-        time.sleep(2)
-        cls()
-        consulta()
+        erase()
+        try:
+            erase()
+            isbn = input('Leia o código de barras\n')
+            query = self.cur.execute(f'SELECT descricao, valor, qtde FROM estoque where isbn={isbn}')
+            result = query.fetchone()
+            cls()
+            print(f'Título: {result[0]}\nPreço: R$ {result[1]}\nEstoque: {result[2]}')
+            input('Pressione enter para voltar')
+            cls()
+            menu()
+        except:
+            print('Código de barras inválido. Tente novamente.')
+            time.sleep(2)
+            cls()
+            self.menu_consulta()
+
+
+    def titulo(self):
+        try:
+            erase()
+            str = input('Digite o título: ')
+            query = self.cur.execute(f"SELECT ISBN, DESCRICAO, QTDE, VALOR FROM ESTOQUE WHERE DESCRICAO LIKE '%{str}%'")
+            result = query.fetchall()
+            cls()
+            for item in result:
+                print(f'ISBN: {item[0]}')
+                print(f'Título: {item[1]}')
+                print(f'Estoque: {item[2]}')
+                print(f'Preço: R$ {item[3]}')
+                print()
+            input('Pressione enter para sair')
+            menu()
+        except:
+            print('Erro: Tente novamente')
+            time.sleep(2)
+            self.titulo()
+
+
 
 
 class Venda:
@@ -50,7 +97,7 @@ class Venda:
         self.menu_venda()
 
     def menu_venda(self):
-        print(self.vendaid)
+        print(f'Pedido n. {self.vendaid}')
         erase()
         print()
         print('Produtos no pedido:')
@@ -87,7 +134,7 @@ class Venda:
         erase()
         try:
             prod = input('Leia o código de barras\n')
-            item = cur.execute(f'SELECT ISBN, VALOR FROM ESTOQUE WHERE ISBN={prod}')
+            item = cur.execute(f'SELECT ISBN, VALOR, QTDE FROM ESTOQUE WHERE ISBN={prod}')
             result = item.fetchall()[0]
             quant = input('Digite a quantidade:\n')
             if quant == '':
@@ -95,6 +142,8 @@ class Venda:
             total = float(result[1]) * float(quant)
             cur.execute(f'INSERT INTO ITEMVENDA(ISBN, QUANTIDADE, VALOR, TOTAL, VENDA) VALUES({result[0]}, {quant}, {result[1]}, {total}, {self.vendaid})')
             cls()
+            estoque = result[2]
+            cur.execute(f'UPDATE ESTOQUE SET QTDE={int(estoque) - int(quant)} WHERE ISBN={prod}')
             self.menu_venda()
         except:
             print('Código de barras inválido. Tente novamente.')
@@ -115,7 +164,7 @@ class Venda:
             self.total = total
 
     def finaliza_venda(self):
-        cur.execute(f'INSERT INTO VENDA(total) VALUES({self.total})')
+        cur.execute(f'UPDATE VENDA SET TOTAL = {self.total} WHERE ID={self.vendaid}')
         print('''Qual a forma de pagamento?\n\n
                 1. Dinheiro\n
                 2. Cartão\n
